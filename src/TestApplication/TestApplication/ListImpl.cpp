e495afdfa86305c189963fe00671732505a27d9c
amit.kumar <amitkumar.7417@gmail.com>
2020-05-20 00:51:58 +0800

    [ListImpl] Code for finding middle of the list

--- src/TestApplication/TestApplication/ListImpl.cpp
+++ src/TestApplication/TestApplication/ListImpl.cpp
@@ -99,3 +99,28 @@ bool ListImpl::DeleteNode(int val) {
 	cout << "Node cannot be found in the list : " << val << endl;
 	return false;
 }
+
+int ListImpl::FindMiddleOfList() {
+
+	if (head == nullptr) {
+		cout << "Empty Linked List" << endl;
+		return -1;
+	}
+
+	//Use slowptr(step one node at a time) and fastptr(step two node at a time)
+	//When fastprt pointing to the last node or beyond, slowptr will be pointing to the middle of the node.
+	ListNode *slowptr = nullptr, *fastptr = nullptr;
+	slowptr = head;
+	fastptr = head;
+	while (slowptr && fastptr) {
+
+		fastptr = fastptr->next;
+		if (!fastptr)
+			break;
+
+		fastptr = fastptr->next;
+		slowptr = slowptr->next;		
+	}
+
+	return slowptr->data;
+}
